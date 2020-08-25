54e430d22fa9d0f5bba87296b118a29957820bff
Thomas Brown <browthom@mail.gvsu.edu>
2020-08-24 20:22:34 -0400

    Last commit for the night 8/24/20 practicing Java Coding

--- src/top/AP1.java
+++ src/top/AP1.java
@@ -1,5 +1,22 @@
 package top;
 
 public class AP1 {
-
+	
+	/**
+	 * https://codingbat.com/prob/p146974
+	 * Given an array of scores, return true if each score 
+	 * is equal or greater than the one before. The array 
+	 * will be length 2 or more.
+	 * @param scores
+	 * @return
+	 */
+	public boolean scoresIncreasing(int[] scores) {
+	  for (int i = 0; i+1 < scores.length; i++) {
+	    if (!(scores[i] <= scores[i+1])) {
+	      return false;
+	    }
+	  }
+	  
+	  return true;
+	}
 }
--- src/top/String1.java
+++ src/top/String1.java
@@ -280,5 +280,340 @@ public class String1 {
 	public String nTwice(String str, int n) {
 	  return str.substring(0, n) + str.substring(str.length()-n, str.length());
 	}
+	
+	/**
+	 * https://codingbat.com/prob/p139075
+	 * Given a string, return true if "bad" appears starting 
+	 * at index 0 or 1 in the string, such as with "badxxx" 
+	 * or "xbadxx" but not "xxbadxx". The string may be any 
+	 * length, including 0. Note: use .equals() to compare 2 
+	 * strings.
+	 * @param str
+	 * @return
+	 */
+	public boolean hasBad(String str) {
+	  if (str.length() >= 3) {
+	    if (str.substring(0,3).equals("bad")) {
+	      return true;
+	    }
+	  }
+	  
+	  if (str.length() >= 4) {
+	    if (str.substring(1,4).equals("bad")) {
+	      return true;
+	    }
+	  }
+	  
+	  return false;
+	}	
+	
+	/**
+	 * https://codingbat.com/prob/p132118
+	 * Given two strings, append them together 
+	 * (known as "concatenation") and return the result. 
+	 * However, if the concatenation creates a double-char, 
+	 * then omit one of the chars, so "abc" and "cat" yields 
+	 * "abcat".
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public String conCat(String a, String b) {
+	  if (a.length() > 0 && b.length() > 0) {
+	    if (a.charAt(a.length()-1) == b.charAt(0)) {
+	      return a + b.substring(1,b.length());
+	    } else {
+	      return a + b;
+	    }
+	  } else {
+	    return a + b;
+	  }
+	}
 
+	/**
+	 * https://codingbat.com/prob/p196652
+	 * Given a string, return true if the first 2 chars 
+	 * in the string also appear at the end of the string, 
+	 * such as with "edited".
+	 * @param str
+	 * @return
+	 */
+	public boolean frontAgain(String str) {
+	  if (str.length() >= 2) {
+	    if (str.substring(0,2).equals(str.substring(str.length()-2,str.length()))) {
+	      return true;
+	    } else {
+	      return false;
+	    }
+	  } else {
+	    return false;
+	  }
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p142247
+	 * Given a string, if a length 2 substring appears at 
+	 * both its beginning and end, return a string without 
+	 * the substring at the beginning, so "HelloHe" yields 
+	 * "lloHe". The substring may overlap with itself, so 
+	 * "Hi" yields "". Otherwise, return the original string 
+	 * unchanged.
+	 * @param str
+	 * @return
+	 */
+	public String without2(String str) {
+	  if (str.length() >= 3) {
+	    if (str.substring(0,2).equals(str.substring(str.length()-2,str.length()))) {
+	      return str.substring(2,str.length());
+	    } else {
+	      return str;
+	    }
+	  } else if (str.length() == 2) {
+	    return "";
+	  } else {
+	    return str;
+	  }
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p151940
+	 * Given a string, if the first or last chars are 'x', 
+	 * return the string without those 'x' chars, and 
+	 * otherwise return the string unchanged.
+	 * @param str
+	 * @return
+	 */
+	public String withoutX(String str) {
+	  if (str.length() > 1) {
+	    if (str.charAt(0) == 'x') {
+	      str = str.substring(1,str.length());
+	    }
+	  } else if (str.equals("x")) {
+	    return "";
+	  }
+	    
+	  if (str.length() > 1) {
+	    if (str.charAt(str.length()-1) == 'x') {
+	      str = str.substring(0,str.length()-1);
+	    }
+	  } else if (str.equals("x")) {
+	    return "";
+	  }
+	  
+	  return str;
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p147483
+	 * The web is built with HTML strings like "<i>Yay</i>" 
+	 * which draws Yay as italic text. In this example, 
+	 * the "i" tag makes <i> and </i> which surround the 
+	 * word "Yay". Given tag and word strings, create the 
+	 * HTML string with tags around the word, e.g. 
+	 * "<i>Yay</i>".
+	 * @param tag
+	 * @param word
+	 * @return
+	 */
+	public String makeTags(String tag, String word) {
+	  return "<" + tag + ">" + word + "</" + tag + ">";
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p163411
+	 * Given a string, return the string made of its first 
+	 * two chars, so the String "Hello" yields "He". 
+	 * If the string is shorter than length 2, 
+	 * return whatever there is, so "X" yields "X", 
+	 * and the empty string "" yields the empty string "". 
+	 * Note that str.length() returns the length of a string.
+	 * @param str
+	 * @return
+	 */
+	public String firstTwo(String str) {
+	  if (str.length() >= 2) {
+	    return str.substring(0,2);
+	  } else {
+	    return str;
+	  }
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p168564
+	 * Given 2 strings, a and b, return a string of the 
+	 * form short+long+short, with the shorter string on 
+	 * the outside and the longer string on the inside. 
+	 * The strings will not be the same length, but they 
+	 * may be empty (length 0).
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public String comboString(String a, String b) {
+	  if (a.length() > b.length()) {
+	    return b + a + b;
+	  } else {
+	    return a + b + a;
+	  }
+	}
+
+	/**
+	 * https://codingbat.com/prob/p130781
+	 * Given a string, return a "rotated right 2" version 
+	 * where the last 2 chars are moved to the start. 
+	 * The string length will be at least 2.
+	 * @param str
+	 * @return
+	 */
+	public String right2(String str) {
+	  if (str.length() >= 3) {
+	    return str.substring(str.length()-2, str.length()) + str.substring(0,str.length()-2);
+	  } else {
+	    return str;
+	  }
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p137729
+	 * Given a string of even length, return a string made 
+	 * of the middle two chars, so the string "string" 
+	 * yields "ri". The string length will be at least 2.
+	 * @param str
+	 * @return
+	 */
+	public String middleTwo(String str) {
+	  return str.substring(str.length() / 2 - 1, str.length() / 2 + 1);
+	}
+
+	/**
+	 * https://codingbat.com/prob/p144623
+	 * Given a string and an index, return a string length 
+	 * 2 starting at the given index. If the index is too 
+	 * big or too small to define a string length 2, use 
+	 * the first 2 chars. The string length will be at 
+	 * least 2.
+	 * @param str
+	 * @param index
+	 * @return
+	 */
+	public String twoChar(String str, int index) {
+	  if (index < str.length()-1 &&
+	      index >= 0) {
+	    return str.substring(index,index+2);
+	  } else {
+	    return str.substring(0,2);
+	  }
+	}
+
+	/**
+	 * https://codingbat.com/prob/p139076
+	 * Given a string, return a string length 2 
+	 * made of its first 2 chars. If the string 
+	 * length is less than 2, use '@' for the missing chars.
+	 * @param str
+	 * @return
+	 */
+	public String atFirst(String str) {
+	  if (str.length() >= 2) {
+	    return str.substring(0,2);
+	  } else if (str.length() == 1) {
+	    return str + "@";
+	  } else {
+	    return "@@";
+	  }
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p194786
+	 * Given a string of any length, return a new string 
+	 * where the last 2 chars, if present, are swapped, 
+	 * so "coding" yields "codign".
+	 * @param str
+	 * @return
+	 */
+	public String lastTwo(String str) {
+	  if (str.length() >= 3) {
+	    return str.substring(0,str.length()-2) + 
+	           str.charAt(str.length()-1) +
+	           str.charAt(str.length()-2);
+	  } else if (str.length() == 2) {
+	    return str.substring(str.length()-1, str.length()) +
+	           str.substring(str.length()-2, str.length()-1);
+	  } else {
+	    return str;
+	  }
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p105745
+	 * Given two strings, append them together 
+	 * (known as "concatenation") and return the result.
+	 * However, if the strings are different lengths, 
+	 * omit chars from the longer string so it is the 
+	 * same length as the shorter string. So "Hello" 
+	 * and "Hi" yield "loHi". The strings may be any length.
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public String minCat(String a, String b) {
+	  if (a.length() > b.length()) {
+	    return a.substring(a.length()-b.length(),a.length()) + b;
+	  } else if (a.length() < b.length()) {
+	    return a + b.substring(b.length()-a.length(),b.length());
+	  } else {
+	    return a + b;
+	  }
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p110141
+	 * Given a string, return a version without the first 
+	 * 2 chars. Except keep the first char if it is 'a' 
+	 * and keep the second char if it is 'b'. The string 
+	 * may be any length. Harder than it looks.
+	 * @param str
+	 * @return
+	 */
+	public String deFront(String str) {    
+	  if (str.length() >= 3) {
+	    return (str.charAt(0)=='a' ? "a" : "") +
+	           (str.charAt(1)=='b' ? "b" : "") +
+	           str.substring(2,str.length());
+	           
+	  } else if (str.length() == 2) {
+	    return (str.charAt(0)=='a' ? "a" : "") +
+	           (str.charAt(1)=='b' ? "b" : "");
+	           
+	  } else if (str.length() == 1) {
+	    return (str.charAt(0)=='a' ? "a" : "");
+	    
+	  } else {
+	    return "";
+	  }
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p151359
+	 * Given a string, if one or both of the first 2 chars 
+	 * is 'x', return the string without those 'x' chars, 
+	 * and otherwise return the string unchanged. This is 
+	 * a little harder than it looks.
+	 * @param str
+	 * @return
+	 */
+	public String withoutX2(String str) {
+	  if (str.length() >= 3) {
+	    return (str.charAt(0) == 'x' ? "" : str.substring(0,1)) +
+	           (str.charAt(1) == 'x' ? "" : str.substring(1,2)) +
+	           str.substring(2,str.length());
+	  } else if (str.length() == 2) {
+	    return (str.charAt(0) == 'x' ? "" : str.substring(0,1)) +
+	           (str.charAt(1) == 'x' ? "" : str.substring(1,2));
+	  } else if (str.length() == 1) {
+	    return (str.charAt(0) == 'x' ? "" : str.substring(0,1));
+	  } else {
+	    return "";
+	  }
+	}
 }
