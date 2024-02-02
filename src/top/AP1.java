a49faa231fe98e0436caa664b3702cb6041c0339
Thomas Brown <browthom93@gmail.com>
2024-02-02 18:52:43 -0500

    More coding

--- src/top/AP1.java
+++ src/top/AP1.java
@@ -1,4 +1,5 @@
 package top;
+import java.util.*;
 
 public class AP1 {
 	
@@ -13,30 +14,95 @@ public class AP1 {
 	 * @return
 	 */
 	public boolean scoresIncreasing(int[] scores) {
-	  for (int i = 0; i+1 < scores.length; i++) {
-	    if (!(scores[i] <= scores[i+1])) {
-	      return false;
-	    }
-	  }
+		  
+		for (int i = 0; i < scores.length; i++) {
+			if (i > 0) {
+				if (!(scores[i-1] <= scores[i])) {
+					return false;
+				}
+			}
+		}
 	  
-	  return true;
+		return true;
 	}
 	
-	public int commonTwo(String[] a, String[] b) {
-	  int sum = 0, i = 0, j = 0;
+	/**
+	 * https://codingbat.com/prob/p123837
+	 * Given an array of scores, compute the int average of 
+	 * the first half and the second half, and return whichever 
+	 * is larger. We'll say that the second half begins at 
+	 * index length/2. The array length will be at least 2. 
+	 * To practice decomposition, write a separate helper 
+	 * method int average(int[] scores, int start, int end) 
+	 * { which computes the average of the elements between 
+	 * indexes start..end. Call your helper method twice to 
+	 * implement scoresAverage(). Write your helper method 
+	 * after your scoresAverage() method in the JavaBat text 
+	 * area. Normally you would compute averages with doubles, 
+	 * but here we use ints so the expected results are exact.
+	 * @param scores
+	 * @return
+	 */
+	public int scoresAverage(int[] scores) {
+		int aveFirst = average(scores, 0, scores.length / 2);
+		int aveSecond = average(scores, scores.length / 2, scores.length);
 	  
-	  while(i < a.length && j < b.length) {
-	    if (a[i].compareTo(b[j]) < 0) {
-	      i++;
-	    } else if (a[i].compareTo(b[j]) > 0) {
-	      j++;
-	    } else {
-	      sum++;
-	      i++;
-	      j++;
-	    }
-	  }
-	  
-	  return sum;
+		return (aveFirst > aveSecond) ? (aveFirst) : (aveSecond);
 	}
+	
+	private int average(int[] scores, int start, int end) {
+		int sum = 0;
+		for (int i = start; i < end; i++) {
+			sum += scores[i];
+		}
+		  
+		return sum / (end - start);
+	}	
+	
+	/**
+	 * https://codingbat.com/prob/p183407
+	 * Given an array of strings, return a new List 
+	 * (e.g. an ArrayList) where all the strings of 
+	 * the given length are omitted. See wordsWithout() 
+	 * below which is more difficult because it uses arrays.
+	 * @param words
+	 * @param len
+	 * @return
+	 */
+	public List wordsWithoutList(String[] words, int len) {
+		ArrayList<String> newWords = new ArrayList<String>();
+ 
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].length() != len) {
+				newWords.add(words[i]);
+			}
+		}
+  
+		return newWords;  
+	}
+	
+	/**
+	 * https://codingbat.com/prob/p134174
+	 * Given an array of positive ints, return a new array 
+	 * of length "count" containing the first even numbers 
+	 * from the original array. The original array will 
+	 * contain at least "count" even numbers.
+	 * @param nums
+	 * @param count
+	 * @return
+	 */
+	public int[] copyEvens(int[] nums, int count) {
+		int[] evens = new int[count];
+		int j = 0;
+	  
+		for (int i = 0; i < nums.length && j < count; i++) {
+			if (nums[i] % 2 == 0) {
+				evens[j] = nums[i];
+				j++;
+			}
+		}
+	  
+		return evens;
+	}
+
 }
